a219f5be78e0afb43a204bd9f05555407c1d6809
PushEax <pusheax.vk@gmail.com>
2025-03-15 22:11:48 +0200

    add some data

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const ways = [
+  {
+    title: 'Lorem Ipsum 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    title: 'Lorem Ipsum 2',
+    description:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    title: 'Lorem Ipsum 3',
+    description:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  },
+  {
+    title: 'Lorem Ipsum 4',
+    description:
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+];
