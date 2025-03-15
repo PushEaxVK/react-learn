496feecffb66ea546acdd5dec99b20471b148588
PushEax <pusheax.vk@gmail.com>
2025-03-15 22:12:08 +0200

    add logo for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import Time from './Time';
+import vite from '/vite.svg';
 
 export default function Header() {
   return (
     <header>
+      <img src={vite} alt="vite logo" />
       <h3>Welcome!</h3>
 
       <span>
