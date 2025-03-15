ffe706d59e21820d94ef6f7c9bb70e6f1fb9841e
PushEax <pusheax.vk@gmail.com>
2025-03-15 21:36:13 +0200

    change time on interval

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react';
+
 export default function Header() {
+  const [now, setNow] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNow(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <header>
       <h3>Welcome!</h3>
 
-      <span>Час зараз: {new Date().toLocaleTimeString()}</span>
+      <span>Час зараз: {now}</span>
     </header>
   );
 }
