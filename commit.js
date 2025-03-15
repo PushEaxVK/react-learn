97c9cecc621e643751205f8687c2032e4be08498
PushEax <pusheax.vk@gmail.com>
2025-03-15 17:57:39 +0200

    add prettier config

--- commit.js
+++ commit.js
@@ -2,7 +2,9 @@ import { execSync } from 'child_process';
 import readline from 'readline';
 
 const rl = readline.createInterface({
+  // eslint-disable-next-line no-undef
   input: process.stdin,
+  // eslint-disable-next-line no-undef
   output: process.stdout,
 });
 
