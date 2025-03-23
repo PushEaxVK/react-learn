a90223b1d15ea801f5d51df4de6ac33602a44676
PushEax <pusheax.vk@gmail.com>
2025-03-23 03:59:04 +0200

    simple validation

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import clsx from 'clsx';
 import css from './Button.module.css';
 
-export default function Button({ children, onClick, isActive = false }) {
+export default function Button({ children, isActive = false, ...props }) {
   const className = clsx(css.button, isActive && css.active);
   return (
-    <button className={className} onClick={onClick}>
+    <button {...props} className={className}>
       {children}
     </button>
   );
--- src/components/FeedbackSection/FeedbackSection.jsx
+++ src/components/FeedbackSection/FeedbackSection.jsx
@@ -5,9 +5,15 @@ import { useState } from 'react';
 const FeedbackSection = () => {
   const [name, setName] = useState('');
   const [reason, setReason] = useState('help');
+  const [hasError, setHasError] = useState(false);
 
   const handleNameChange = event => {
     setName(event.target.value);
+    setHasError(event.target.value.length === 0);
+  };
+
+  const handleReasonCHange = event => {
+    setReason(event.target.value);
   };
 
   return (
@@ -24,16 +30,30 @@ const FeedbackSection = () => {
           value={name}
           onChange={handleNameChange}
           autoComplete="off"
+          style={{
+            border: hasError ? '1px solid red' : null,
+          }}
         />
 
         <label htmlFor="reason">Reson</label>
-        <select id="reason" className="control">
+        <select
+          id="reason"
+          className="control"
+          value={reason}
+          onChange={handleReasonCHange}
+        >
           <option value="error">Error</option>
           <option value="help">Need help</option>
           <option value="suggest">Suggest</option>
         </select>
 
-        <Button>Send</Button>
+        <pre>
+          Name: {name}
+          <br />
+          Reason: {reason}
+        </pre>
+
+        <Button disabled={hasError}>Send</Button>
       </form>
     </section>
   );
