7aecc970668eefb55580411af45b1aef17ceb393
PushEax <pusheax.vk@gmail.com>
2025-03-23 23:59:32 +0200

    show modal

--- src/components/EffectsSection/EffectsSection.jsx
+++ src/components/EffectsSection/EffectsSection.jsx
@@ -1,8 +1,12 @@
+import { useEffect, useState } from 'react';
 import Button from '../Button/Button';
+import Modal from '../Modal/Modal';
 
 const EffectsSection = () => {
+  const [modal, setModal] = useState(false);
+
   const openModal = () => {
-    alert('Opening modal');
+    setModal(true);
   };
 
   return (
@@ -10,6 +14,17 @@ const EffectsSection = () => {
       <h3>Effects</h3>
 
       <Button onClick={openModal}>Open info</Button>
+
+      <Modal open={modal}>
+        <h3>Hello from modal!</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni aut
+          repudiandae, fugit eum enim alias accusamus cupiditate temporibus
+          aperiam sint? Ducimus eligendi in voluptatum et excepturi eaque
+          dolorem totam eius?
+        </p>
+        <Button onClick={() => setModal(false)}>Close modal!</Button>
+      </Modal>
     </section>
   );
 };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,22 @@
-const Modal = () => {
-  return <dialog></dialog>;
+import './Modal.css';
+import { createPortal } from 'react-dom';
+import { useRef, useEffect } from 'react';
+
+const Modal = ({ children, open }) => {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
+
+  return createPortal(
+    <dialog ref={dialog}>{children}</dialog>,
+    document.getElementById('modal')
+  );
 };
+
 export default Modal;
